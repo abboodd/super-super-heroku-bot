7e5a7ab00cf50b242f9c4d027187a8a7b80179f5
abboodd <38141887+abboodd@users.noreply.github.com>
2018-04-07 08:57:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(NDMxODk0MDg0Mzk1ODU5OTY4.DanoLw.zo8YDr9BGxPhK-UxHD1rvzC6or0);
